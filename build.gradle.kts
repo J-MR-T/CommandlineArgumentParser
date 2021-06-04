ff3e161829a36ee378afd971bbeb99c5eabb71b2
Jim M. R. Teichgräber <jimmr.teichgraeber@tum.de>
2021-06-04 16:19:28 +0200

    Add maven publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("io.kotest:kotest-runner-junit5-jvm:4.6.0")
     testImplementation(kotlin("test"))
     implementation(kotlin("reflect"))
+    implementation("com.github.jitpack:gradle-simple:1.0")
 }
 
 
